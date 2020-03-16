5b5af073b5dff85dc2f84103850edd3e0c678e8d
Corey Lux <50592788+CLux17@users.noreply.github.com>
2020-03-16 19:18:00 +0000

    Add project files.

---
+++ EmplyeesApi/Controllers/StatusController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmplyeesApi.Controllers
+{
+    public class StatusController : Controller
+    {
+        [HttpGet("/status")]
+        public ActionResult GetTheServerStatus()
+        {
+            var response = new StatusResponse
+            {
+                Status = "Everything is Running Smoothly",
+                CreatedAt = DateTime.Now
+            };
+            return Ok(response);
+        }
+
+        [HttpGet("/blogs/{year:int}/{month:int}/{day:int}")]
+        public ActionResult GetBlogPosts(int year, int month, int day)
+        {
+            return Ok($"Getting you the blog posts for: {month}/{day}/{year}");
+        }
+    }
+
+    public class StatusResponse
+    {
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
